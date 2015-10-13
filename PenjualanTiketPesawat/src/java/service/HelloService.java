7ea30c4afbdf804ad9ccfeb55ee1e8df9675995a
LabGSG_36 <LabGSG_36@LabGSG36-PC>
2015-10-13 09:28:50 +0700

    13/10/2015Form

---
+++ PenjualanTiketPesawat/src/java/service/HelloService.java
@@ -0,0 +1,20 @@
+package service;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LabGSG
+ */
+public class HelloService {
+    public static String sayHello(String name){
+        return "Hello " + name ;
+    }
+    public static String sayAge(String age){
+        return " Your Age : " + age ;
+    }
+}
